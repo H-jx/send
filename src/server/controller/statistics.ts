d17d495d57688c4bf608515dc43541fa26a541c7
hb-bobo <751968840@qq.com>
2022-06-19 20:16:36 +0800

    feat: Statistics

---
+++ src/server/controller/statistics.ts
@@ -0,0 +1,34 @@
+import { Context } from 'koa'
+import { RPCMethod, RPCService } from 'ts-brpc/server'
+import { logger } from '../common/logger'
+import { errorJSON, successJSON } from '../common/response'
+import { ResponseBody } from '../interface/Http'
+
+import { getCurrentRoomts, getUserList } from '../service/statistics'
+
+@RPCService()
+export class Statistics {
+  ctx!: Context
+  @RPCMethod()
+  async getCurrentRoomts(): Promise<ResponseBody<string[]>> {
+    try {
+      const rooms = await getCurrentRoomts()
+
+      return successJSON({ data: rooms })
+    } catch (error) {
+      logger.error(error)
+      return errorJSON({ message: (error as Error).message })
+    }
+  }
+
+  @RPCMethod()
+  async getUserList(): Promise<ResponseBody<string[]>> {
+    try {
+      const data = await getUserList()
+      return successJSON({ data: data })
+    } catch (error) {
+      logger.error(error)
+      return errorJSON()
+    }
+  }
+}
